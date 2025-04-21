b5937d11d4dbbffa11ebee8419171b1a6e77fdae
Roka <kazakov302215@gmail.com>
2025-04-20 18:35:58 -0600

    Added coins

--- src/components/Scene/MainContainer/MainContainer.tsx
+++ src/components/Scene/MainContainer/MainContainer.tsx
@@ -5,10 +5,12 @@ import { Texture } from '@pixi/core'
 import { Camera } from '../../Camera/Camera'
 import { Level } from '../../Levels/Level'
 import { Hero } from '../../Hero/Hero'
+import { Coin } from '../../Coin/Coin'
 import { TILE_SIZE } from '../../../constants/game-world'
 
 import backgroundAsset from '../../../assets/space.jpg'
 import herodAsset from '../../../assets/hero.png'
+import coinAsset from '../../../assets/coin-gold.png'
 
 interface MainContainerProps {
   canvasSize: { width: number; height: number }
@@ -19,6 +21,7 @@ export const MainContainer = ({ canvasSize, children }: PropsWithChildren<MainCo
 
   const backgroundTexture = useMemo(() => Texture.from(backgroundAsset), [])
   const heroTexture = useMemo(() => Texture.from(herodAsset), [])
+  const coinTexture = useMemo(() => Texture.from(coinAsset), [])
 
   const updateHeroPosition = useCallback((x: number, y: number) => {
     setHeroPosition({ x: Math.floor(x / TILE_SIZE), y: Math.floor(y / TILE_SIZE) })
@@ -31,6 +34,8 @@ export const MainContainer = ({ canvasSize, children }: PropsWithChildren<MainCo
       <Camera heroPosition={heroPosition} canvasSize={canvasSize}>
         <Level />
         <Hero texture={heroTexture} onMove={updateHeroPosition} />
+        <Coin texture={coinTexture} x={5} y={10} />
+        <Coin texture={coinTexture} x={6} y={11} />
       </Camera>
     </Container>
   )
